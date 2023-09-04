9cd0e453a788a48f9bd7cd6dca1108d37d9c3194
Ernesto Neto <ntoernesto123@gmail.com>
2023-09-04 13:19:25 -0300

    ajuste no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,13 +4,13 @@ import * as S from './styles'
 import { useAuth } from '@/contexts/auth'
 
 const Header = () => {
-  const {user, logout} = useAuth()
+  const auth = useAuth()
 
   return (
     <S.Container>
-      <span>{user.name}</span>
+      <span>{auth?.user.name}</span>
 
-      <button onClick={logout}>Sair</button>
+      <button onClick={auth?.logout}>Sair</button>
     </S.Container>
   )
 }

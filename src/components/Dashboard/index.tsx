49c24da2d2d21272edc38497fdbceea768406f26
Ernesto Neto <ntoernesto123@gmail.com>
2023-09-04 18:53:34 -0300

    criação da pagina dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,15 +1,137 @@
-'use client'
-import React from 'react'
-import * as S from './styles'
+"use client";
+import React from "react";
+import * as S from "./styles";
+import {FiEdit2, FiTrash} from 'react-icons/fi'
 
+import {doc, addDoc, getDocs, query, collection, where, deleteDoc, updateDoc} from 'firebase/firestore'
+import { db } from "@/services/firebase_conection";
+import { useAuth } from "@/contexts/auth";
 
-const Dashboard = () => {
-  return (
-    <S.Container>
-      <textarea />
-      <button>Salvar</button> 
-    </S.Container>
-  )
+type TaskProps = {
+  task: string
+  id: string
 }
 
-export default Dashboard
+const Dashboard = () => {
+  const [tasks, setTasks] = React.useState<TaskProps[]>([])
+  const [input, setInput] = React.useState('')
+  const [loading, setLoading] = React.useState(false)
+
+  const [itemToEdit, setItemToEdit] = React.useState(null)
+
+  const auth = useAuth()
+
+  const uid = auth && auth.user.uid
+
+  React.useEffect(() => {
+    const loadTasks = async () => {
+
+      const collectionRef = collection(db, 'tasks')
+      const q = query(collectionRef, where('userUid', '==', uid))
+
+      await getDocs(q)
+      .then((snapshot) => {
+        let data: TaskProps[] | [] = []
+
+        snapshot.forEach(item => {
+          return data.push({
+            task: item.data().task,
+            id: item.id
+          });
+        })
+
+        setTasks(data)
+      })
+    }
+
+    loadTasks()
+  }, [tasks, uid])
+
+  const handleSaveTask = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setLoading(true)
+
+
+    if(itemToEdit){
+      const docRef = doc(db, 'tasks', itemToEdit)
+      await updateDoc(docRef, {
+        task: input
+      })
+      .then(() => {
+        setInput('')
+        setLoading(false)
+      })
+
+      return 
+    }
+
+
+    const collectionRef = collection(db, 'tasks')
+    await addDoc(collectionRef, {
+      task: input,
+      userUid: auth?.user.uid
+    })
+    .then(() => {
+      setInput('')
+      setLoading(false)
+    })
+  }
+
+  const handleDeleteTask = async (id: string) => {
+    const docRef = doc(db, 'tasks', id)
+    
+    await deleteDoc(docRef)
+  }
+
+  const toEditing = (data) => {
+    setInput(data.task)
+    setItemToEdit(data.id)
+  }
+
+  return (
+    <S.Container>
+      <S.Form className="form" onSubmit={handleSaveTask}>
+        <textarea
+          placeholder='Digite sua tarefa'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button
+          type='submit'
+        >{loading ? (
+          <span className='loading'>
+          Carregando
+          </span>
+        ): (
+          <>
+            {!!itemToEdit ? 'Atualizar' : 'Salvar'}
+          </>
+        )}
+        </button>
+      </S.Form>
+      <S.Tasks>
+
+        {tasks.length === 0 && (<p>{"Sem tarefas por aqui :'("}</p>)}
+
+        {tasks.map((item, index) => (
+          <p key={index}>
+          <span>{item.task}</span>
+          
+          <div className="controls">
+            <button onClick={() => toEditing(item)}>
+              <FiEdit2 />
+            </button>
+            <button onClick={() => handleDeleteTask(item.id)}>
+              <FiTrash />
+            </button>
+          </div>
+        </p>
+        ))}
+
+      </S.Tasks>
+    </S.Container>
+  );
+};
+
+export default Dashboard;
--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -1,30 +1,98 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.main`
-    width: 100%;
-    height: 100vh;
-    padding-top: 2.5rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  width: 100%;
+  height: 100vh;
+  padding-top: 2.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-    textarea{
-        width: 80% !important;
-        height: 300px;
-        border-radius: 8px;
-        resize: none;
-        outline: 0;
-        padding: .8rem;
-    }
+export const Form = styled.form`
+  width: 90%;
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-    button{
+  textarea {
+    width: 80% !important;
+    height: 300px;
+    border-radius: 8px;
+    resize: none;
+    outline: 0;
+    padding: 0.8rem;
+  }
+
+  button {
+    margin-top: 1rem;
+    width: 80%;
+    padding: 12px;
+    background-color: #6622cc;
+    color: #fafafa;
+    border: 0;
+    border-radius: 8px;
+    font-size: 1.2rem;
+    cursor: pointer;
+  }
+`;
+
+export const Tasks = styled.section`
         margin-top: 1rem;
-        width: 80%;
-        padding: 8px;
-        background-color: #6622cc;
-        color: #fafafa;
-        border: 0;
-        border-radius: 8px;
-        font-size: 1.2rem;
+        width: 90%;
+        max-width: 800px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 8px;
+        
+        p{
+            width: 80%;
+            background-color: #fafafa;
+            padding: 1rem;
+            border-radius: 8px;
+            display: flex;
+            justify-content: space-between;
+    
+            .controls{
+                display: flex;
+                gap: 8px;
+
+                & > :nth-child(1){
+                    background-color: #c38eb4;
+                    color: #fafafa;
+                }
+
+                & > :nth-child(2){
+                    background-color: #26425a;
+                    color: #fafafa;
+                }
+            }
+
+            button{
+                padding: 4px;
+                border: 0;
+                border-radius: 8px;
+                cursor: pointer;
+                transition: all ease-in-out .3s;
+
+                &:hover{
+                    transform: scale(1.2) rotate(20deg);
+                }
+            }
+        }
     }
-`
+
+    .loading{
+        &::after{
+            content: ' ';
+            animation: elipse 2s infinite;
+        }
+    }
+
+    @keyframes elipse {
+        0% {content: '.';}
+        33% {content: '..';}
+        66% {content: '...';}
+`;
